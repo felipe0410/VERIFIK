574b8df773df0d5460cff9c9c9b1203888de5284
Felipe-whitesoft <felipe.montana@whitesoft.com.au>
2023-10-10 22:45:43 -0500

    correction depens

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,13 +1,13 @@
 import { Box } from "@mui/material";
-import Container from "../components/Container/Container";
-import CategoryForm from "./formCategory";
+// import Container from "../components/Container/Container";
+// import CategoryForm from "./formCategory";
 
 const CreateNote = () => {
   return (
     <Box sx={{ color: "#fff" }}>
-      <Container>
+      {/* <Container>
         <CategoryForm />
-      </Container>
+      </Container> */}
     </Box>
   );
 };
